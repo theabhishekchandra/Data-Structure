12666ad7c27f180ad821b943baad24fb81984828
Abhishek Chandra <ac927920@gmail.com>
2025-01-27 23:40:36 +0530

    Add ArrayEasyProblem with multiple array-related solutions
    This commit introduces the `ArrayEasyProblem` class, providing implementations for several array-based problems, including Two Sum, Remove Duplicates, Search Insert Position, Plus One, Best Time to Buy and Sell Stock, Majority Element, Create Target Array, Array Subset, and Find Middle Index in Array. These solutions demonstrate common algorithmic techniques and handle typical array manipulation tasks.

---
+++ src/array/ArrayEasyProblem.java
@@ -0,0 +1,189 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class ArrayEasyProblem {
+    public static void main(String[] args){
+//        int[] nums = {2,7,11,15};
+//        int[] nums = {2,2,2,2,2,2,7,7,7,7,7,11,11,11,11,15,15,15,15,15,15};
+//        int target = 9;
+//        int[] nums = { 1, 3, 5, 6};
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        int target = 7;
+
+        ArrayEasyProblem input = new ArrayEasyProblem();
+
+//        System.out.println("Two Sum : " + Arrays.toString(input.twoSum(nums, target)));
+//        System.out.println("Output : " + input.removeDuplicates(nums));
+//        System.out.println("Output : " + input.searchInsert(nums, target));
+        System.out.println("Output : " + Arrays.toString(input.createTargetArray(nums, index)));
+//        int[] temp = new int[5];
+////        for (int j : temp){
+////            System.out.println("Output : " + j);
+////        }
+////        System.out.println("Output : " + temp);
+
+    }
+    /** 1. Two Sum
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     *
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     *
+     * You can return the answer in any order.*/
+    public int[] twoSum(int[] nums, int target) {
+        int[] indices = new int[2];
+        for (int i =0; i<nums.length;i++){
+            for (int j = i + 1; j< nums.length; j++){
+                if ( target == nums[i] + nums[j]){
+                    indices[0] = i;
+                    indices[1] = j;
+                    return indices;
+                }
+            }
+        }
+        return indices;
+    }
+
+    /**Remove Duplicates from Sorted Array
+     *
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+     *
+     * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+     *
+     * Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+     * Return k.
+     * */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        /*[0,0,1,1,1,1,2,3,3,4]*/
+        /*[0,_,1,_,1,2,2,3,3,4]*/
+        int unique = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] != nums[unique -1]){
+                nums[unique] = nums[i];
+                unique++;
+            }
+        }
+        return unique;
+    }
+    /** Search Insert Position
+     * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     *
+     * You must write an algorithm with O(log n) runtime complexity.
+     * */
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid -1;
+
+            }
+        }
+        return start;
+    }
+    /** 66. Plus One
+     * */
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--){
+            if (digits[i] == 9){
+                digits[i] = 0;
+            }else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    /** 121. Best Time to Buy and Sell Stock
+     * */
+    public int maxProfit(int[] prices) {
+        int maxProfit = Integer.MIN_VALUE;
+        int minPrice = prices[0];
+        for (int i = 1; i< prices.length; i++){
+            int diff = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, diff);
+            minPrice = Math.min(prices[i], minPrice);
+        }
+        return maxProfit;
+    }
+
+    /**
+     * 169. Majority Element
+     */
+    public int majorityElement(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == major) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    major = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return major;
+    }
+    /** 1389. Create Target Array in the Given Order
+     * */
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < index.length; i++) {
+            arr.add(index[i],nums[i]);
+
+        }
+        int[] target = new int[nums.length];
+        for (int i = 0; i < arr.size(); i++) {
+            target[i] = arr.get(i);
+        }
+        return target;
+    }
+    /** GFG -> Array Subset */
+    public boolean isSubset(int a[], int b[]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : a) {
+            set.add(num);
+        }
+        // Check if all elements of b[] are present in the set
+        for (int num : b) {
+            if (!set.contains(num)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 1991. Find the Middle Index in Array
+     */
+    public int findMiddleIndex(int[] nums) {
+        int totalSum = 0, leftSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+}
