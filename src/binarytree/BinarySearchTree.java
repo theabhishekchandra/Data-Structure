0c4e55559599a6dceee748f0d6362f1f46147419
Abhishek Chandra <ac927920@gmail.com>
2025-04-11 14:49:57 +0530

    Update tree.

---
+++ src/binarytree/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package binarytree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+    /** Sorted array to Balanced BST
+     * arr = {3,6,5,8,10,11,12}*/
+}
--- src/binarytree/BinaryTreeBasic.java
+++ src/binarytree/BinaryTreeBasic.java
@@ -202,5 +202,20 @@ public class BinaryTreeBasic {
 
     ///  Lowest common ancestor.
 
+    /// Transform to a sum of a tree.
+    static int transform(Node root){
+        if (root == null){
+            return 0;
+        }
+        int leftChild =  transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild+ newRight + rightChild;
+        return data;
+
+    }
 
 }
